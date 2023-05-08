d3c5f9e4c9fe5e6f35b1cb17c3a936108506249c
Stewart <44583600+stewheart123@users.noreply.github.com>
2023-05-08 10:45:21 +0100

    theme colours in tailwind , code commenting

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+/** Interfaces describe the 'shape' of the data without showing implementation of methods or properties */
 interface ToDoItem {
   id: number;
   description: string;
@@ -12,23 +13,41 @@ interface ToDoList {
 }
 
 function ToDoList() {
-  const [toDoList, setToDoList] = useState<ToDoList | null>(null);
+  // useState - in typescript you can set the type of the state. It will try to infer the value if not defined
+  const [toDoList, setToDoList] = useState<ToDoList | null>(null); // sets the initial state value type
   const [newTaskDescription, setNewTaskDescription] = useState("");
 
+  /**useEffect handles side effects of functional components- in this example however it's designed to only run once
+   * - this creates the empty list if it doesn't already exist.
+   * - we tell it to run once by having an empty array as it's dependencies - it doesn't have anything to monitor changes of.
+   */
   useEffect(() => {
-    const storedToDoList = localStorage.getItem("toDoList");
+    const storedToDoList = localStorage.getItem("toDoList"); // localStorage.getItem() fetches the item if it exists
     if (storedToDoList) {
-      setToDoList(JSON.parse(storedToDoList));
+       /** we have to parse the date back from a string into an object
+        *  then pass it into the state update function 'setToDoList'
+       */
+      setToDoList(JSON.parse(storedToDoList)); 
     } else {
       const newToDoList: ToDoList = {
         tasks: [],
         nextIdNumber: 0,
       };
-      setToDoList(newToDoList);
-      localStorage.setItem("toDoList", JSON.stringify(newToDoList));
+      setToDoList(newToDoList); 
+      localStorage.setItem("toDoList", JSON.stringify(newToDoList));  
+      /**
+       *  localStorage.setItem will create the json string, plus we pass in the stringify function
+      * + the new to do list item object
+       */
     }
   }, []);
 
+  /**
+   * Called when form submit button is clicked
+   * - creates the new task entry, based on the form values
+   * - setToDoList takes the previous state via the '...'
+   * - the '!' is specific to typescript and means "I know this value is not null or undefined, so let me use it as if it's not".
+   */
   const addTask = () => {
     const newTask = {
       id: toDoList!.nextIdNumber++,
@@ -44,6 +63,11 @@ function ToDoList() {
     setNewTaskDescription("");
   };
 
+  /**
+   * This is called when the checkbox is used.
+   * It updates the entire list, mapping the tasks out and setting the one with corresponding id complete bool to opposite
+   * -Note that we are using 'map' without markup in this example - different use case to using it to generate markup. cool!
+   */
   const toggleTask = (taskId: number) => {
     setToDoList((prevState) => {
       const updatedTasks = prevState!.tasks.map((task) => {
@@ -59,6 +83,10 @@ function ToDoList() {
     });
   };
 
+  /**
+   * This removes the corresponding task via the filter method
+   * filter will take all that arent the same as the id and create a new array from them
+   */
   const deleteTask = (taskId: number) => {
     setToDoList((prevState) => {
       const updatedTasks = prevState!.tasks.filter(
@@ -71,6 +99,9 @@ function ToDoList() {
     });
   };
 
+  /**
+   * This looks at the todoList as the dependency and updates the local storage based on the contents
+   */
   useEffect(() => {
     if (toDoList !== null) {
       localStorage.setItem("toDoList", JSON.stringify(toDoList));
@@ -79,9 +110,10 @@ function ToDoList() {
 
   return (
     <React.Fragment>
+      {/* conditional that will either show the to do list, or show 'loading' markup */}
       {toDoList ? (
         <section className="list-container m-auto relative">
-          <div className="logo not-italic font-normal text-3xl leading-none text-gray-900 text-center w-full p-4">
+          <div className="logo not-italic font-normal text-3xl leading-none text-gray-900 text-center w-full p-4 bg-brand-yellow">
             done.
           </div>
           <form
@@ -89,10 +121,10 @@ function ToDoList() {
               event.preventDefault();
               addTask();
             }}
-            className="flex flex-col p-8"
+            className="flex flex-col p-8 bg-brand-pink"
           >
             <input
-              className="list-input h-12 w-full text-3xl pl-4"
+              className="list-input h-12 w-full text-3xl pl-4 bg-brand-grey"
               type="text"
               value={newTaskDescription}
               onChange={(event) => setNewTaskDescription(event.target.value)}
@@ -106,12 +138,14 @@ function ToDoList() {
               +
             </button>
           </form>
-          <div className="list-inner h-full p-8 relative overflow-y-scroll">
+          <div className="list-inner h-full p-8 relative overflow-y-scroll bg-brand-light-grey">
             <ul>
+              {/* this maps out all the items in our to do list. 
+              map is a really useful function for showing an array */}
               {toDoList.tasks.map((task) => (
                 <li
                   key={task.id}
-                  className="not-italic font-normal text-3xl flex items-center"
+                  className="not-italic font-normal text-3xl flex items-center text-brand-blue"
                 >
                   <input
                     type="checkbox"
@@ -125,7 +159,7 @@ function ToDoList() {
                   {task.completed && (
                     <button
                       onClick={() => deleteTask(task.id)}
-                      className="m-0 ml-8"
+                      className="m-0 ml-8 text-brand-red bg-clear"
                     >
                       X
                     </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#2269DA',
-        secondary : '#FDCF25'
-      }
+        "brand-blue": "#2269DA",
+        "brand-yellow": "#FDCF25",
+        "brand-pink": "#EA98A6",
+        "brand-grey": "#D9D9D9",
+        "brand-light-grey": "#E0DFF6",
+        "brand-orange": "#F2502A",
+        'clear': "#FF000000",
+        "brand-red": "#DF2F06",
+      },
     },
   },
   plugins: [],
-}
+};
